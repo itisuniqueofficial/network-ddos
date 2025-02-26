0dec1fd75ed91364cf04a65d6bd5281cbbd134c3
It Is Unique Official <141048868+itisuniqueofficial@users.noreply.github.com>
2025-02-27 00:49:54 +0530

    Delete network_ddos directory

--- network_ddos/__init__.py
+++
@@ -1,7 +0,0 @@
-from .core import execute_test
-from .profiles import AttackProfile, DEFAULT_PROFILES
-from .utils import Metrics
-from .config import load_config
-
-__version__ = "1.0.0"
-__author__ = "It Is Unique Official"
